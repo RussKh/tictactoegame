1a5273b5b853be270902900fd26ae33ed83b94a7
russkh <kharuba@gmail.com>
2024-04-30 00:55:10 -0600

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     new Array(9).fill("button button-trans")
   );
   const [wins, setWins] = useState([]);
-  const [isComputerPlaying, setIsComputerPlaying] = useState(false);
+  const [isComputerPlaying, setIsComputerPlaying] = useState(true);
   const [difficulty, setDifficulty] = useState(difficultyLevel[0]);
 
   let turnCount = squares.filter((square) => square).length;
@@ -32,10 +32,23 @@ function App() {
   function handleDifficulty() {
     let i = difficultyLevel.findIndex((level) => level === difficulty);
     setDifficulty(difficultyLevel[(i + 1) % 3]);
+    setSquares(new Array(9).fill(null));
+    setWinningColors(new Array(9).fill("button button-trans"));
+    setSquares(new Array(9).fill(null));
+    setIsXNext(true);
+    setWinner(null);
   }
+
   function handleComputerPlaying() {
     setIsComputerPlaying(!isComputerPlaying);
-    setWins([]);
+    reset();
+  }
+
+  function reset() {
+    setSquares(new Array(9).fill(null));
+    setIsXNext(true);
+    setWinner(null);
+    setWinningColors(new Array(9).fill("button button-trans"));
   }
 
   useEffect(() => {
@@ -57,21 +70,13 @@ function App() {
   }
 
   function computerMove() {
-    function fiftyFiftyChance() {
-      return Math.random() < 0.7;
-    }
-
-    // if center is vacant, use it as the first move
-    if (!squares[4] && difficulty === "impossible") {
-      handleMove(4);
-      return; // Exit the function after making a move
-    }
-
     // If O is close to winning, finish it
-    if (
-      difficulty === "impossible" ||
-      (difficulty === "hard" && fiftyFiftyChance())
-    )
+    let probability =
+      difficulty === "easy" ? 0.2 : difficulty === "hard" ? 0.7 : 1;
+
+    console.log(probability);
+
+    if (Math.random() <= probability)
       for (let combo of winningCombos) {
         if (combo.filter((el) => squares[el] === "O").length === 2) {
           let ix = combo.find((x) => squares[x] !== "O");
@@ -82,12 +87,8 @@ function App() {
           }
         }
       }
-
-    // // If X is close to winning, block it
-    if (
-      difficulty === "impossible" ||
-      (difficulty === "hard" && fiftyFiftyChance())
-    )
+    // If X is close to winning, block it
+    if (Math.random() <= probability)
       for (let combo of winningCombos) {
         if (combo.filter((el) => squares[el] === "X").length === 2) {
           let ix = combo.find((x) => squares[x] !== "X");
@@ -98,22 +99,61 @@ function App() {
           }
         }
       }
-
-    // Put O in corner with 30% chance
-    if (fiftyFiftyChance()) {
-      for (let corner of [0, 2, 6, 8]) {
-        if (!squares[corner]) {
-          // Check if the position is vacant before making a move
-          handleMove(corner);
-          return; // Exit the function after making a move
+    // if 2 X's are close to one corner block that corner
+    if (Math.random() <= probability) {
+      let edgeCornerPairs = [
+        [0, 1, 3],
+        [2, 1, 5],
+        [6, 3, 7],
+        [8, 5, 7],
+      ];
+      for (let edgeCornerPair of edgeCornerPairs) {
+        if (
+          edgeCornerPair.filter((el) => squares[el] === "X").length === 2 &&
+          edgeCornerPair.find((el) => !squares[el])
+        ) {
+          handleMove(edgeCornerPair.find((el) => !squares[el]));
+          return;
+        }
+      }
+    }
+    // if 2 X's are in opposite corners put O on any edge
+    if (Math.random() <= probability) {
+      if (
+        (squares[0] === squares[8] && squares[0] === "X") ||
+        (squares[2] === squares[6] && squares[2] === "X")
+      ) {
+        for (let edge of [1, 3, 5, 7]) {
+          if (!squares[edge]) {
+            // Check if the position is vacant before making a move
+            handleMove(edge);
+            return; // Exit the function after making a move
+          }
+        }
+      }
+    }
+    // if center is vacant, use it as the first move
+    if (Math.random() <= probability) {
+      if (!squares[4]) {
+        handleMove(4);
+        return; // Exit the function after making a move
+      } else {
+        // if X in center put X in any corner
+        for (let corner of [0, 2, 6, 8]) {
+          if (!squares[corner]) {
+            // Check if the position is vacant before making a move
+            handleMove(corner);
+            return; // Exit the function after making a move
+          }
         }
       }
     }
 
-    // all else put O in any available square
-    for (let i = 0; i < squares.length; i++) {
-      if (!squares[i]) handleMove(i);
-    }
+    // all else put O in any available square randomly
+    let vacantBoxes = squares.map((e, i) => (!e ? i : "")).filter(String);
+    let randomIndex =
+      vacantBoxes[Math.floor(Math.random() * vacantBoxes.length)];
+    handleMove(randomIndex);
   }
 
   function checkWinner(newSquares) {
@@ -138,13 +178,6 @@ function App() {
     }
   }
 
-  function reset() {
-    setSquares(new Array(9).fill(null));
-    setIsXNext(true);
-    setWinner(null);
-    setWinningColors(new Array(9).fill(null));
-  }
-
   useEffect(() => {
     if (turnCount === 9 && !winner)
       setWinningColors([...Array(9).fill("button button-red")]);
@@ -158,11 +191,14 @@ function App() {
           {isComputerPlaying ? "🤖" : "👨‍🦰"}
         </button>
       </div>
-      {isComputerPlaying && (
-        <button className="level" onClick={handleDifficulty}>
-          Level: {difficulty}
-        </button>
-      )}
+
+      <button
+        className={isComputerPlaying ? "level" : "level level-inactive"}
+        onClick={handleDifficulty}
+      >
+        Level: {difficulty}
+      </button>
+
       <Board
         handleMove={handleMove}
         squares={squares}
